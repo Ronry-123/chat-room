567974d226d07c8c973081c7a07e8b372149f2e7
loorr <zjianfa@qq.com>
2022-08-14 15:46:48 +0800

    add

--- chat-room-rest/src/main/java/org/example/chat/room/controller/ChatUserController.java
+++ chat-room-rest/src/main/java/org/example/chat/room/controller/ChatUserController.java
@@ -5,6 +5,7 @@ import org.example.chat.room.ChatUserApi;
 import org.example.chat.room.domain.user.UserVo;
 import org.example.chat.room.domain.user.req.UserLoginReq;
 import org.example.chat.room.domain.user.req.UserSignReq;
+import org.example.chat.room.service.ChatUserService;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,15 +14,17 @@ import javax.annotation.Resource;
 public class ChatUserController implements ChatUserApi {
 
     @Resource
-
+    private ChatUserService chatUserService;
 
     @Override
     public Response<Boolean> userSign(UserSignReq req) {
-        return null;
+        Boolean result = chatUserService.userSign(req);
+        return Response.getOk(result);
     }
 
     @Override
     public Response<UserVo> userLogin(UserLoginReq req) {
-        return null;
+        UserVo result = chatUserService.userLogin(req);
+        return Response.getOk(result);
     }
 }
--- chat-room-rest/src/main/java/org/example/chat/room/service/ChatUserService.java
+++ chat-room-rest/src/main/java/org/example/chat/room/service/ChatUserService.java
@@ -1,4 +1,13 @@
 package org.example.chat.room.service;
 
+import org.example.chat.room.domain.user.UserVo;
+import org.example.chat.room.domain.user.req.UserLoginReq;
+import org.example.chat.room.domain.user.req.UserSignReq;
+
 public interface ChatUserService {
+
+    Boolean userSign(UserSignReq req);
+    UserVo userLogin(UserLoginReq req);
+
+
 }
